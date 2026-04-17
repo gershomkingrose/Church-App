301dfea567779482fa9f1e47cc453552e27ee331
Gershom King Rose J K <119334326+gershomkingrose@users.noreply.github.com>
2026-04-17 14:36:14 +0530

    Delete backend directory

--- backend/src/main/java/com/church/app/ministry/MinistryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.church.app.ministry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface MinistryRepository extends JpaRepository<Ministry, Long> {
-    List<Ministry> findByTenantId(Long tenantId);
-}
